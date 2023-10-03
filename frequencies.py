97d34682170c214e9ee5ac63b1895bdcd26c62db
junhyuk woo <junhyuk.1.woo@kcl.ac.uk>
2023-10-03 16:43:13 +0100

    failed

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,17 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
-    return frequencies
+    for x in items:
+        if (x in frequencies):
+            frequencies[x] +=1
+        else:
+            frequencies[x] = 1
+    print(frequencies)
+
+
+items = ['a', 'a', 'b', 'b', 'b', 'c']
+frequencies(items)
+
+#cannout make str = int
+items = [100, 'Hello', '100', '100', 100]
+frequencies(items)
